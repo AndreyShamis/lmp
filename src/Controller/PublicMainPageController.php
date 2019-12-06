a635c28104413d134fa9e3b283fc24e589c78fd3
Andrey Shamis <lolnik@gmail.com>
2019-12-06 14:36:31 +0200

    MAke default page visible
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>
    Change-Id: I519359e534d3af8f7954f4d8d8168e7e567b788b

--- src/Controller/PublicMainPageController.php
+++ src/Controller/PublicMainPageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PublicMainPageController extends AbstractController
 {
     /**
-     * @Route("/public/main/page", name="public_main_page")
+     * @Route("/", name="public_main_page")
      */
     public function index()
     {
